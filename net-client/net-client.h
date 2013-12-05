fb463d7601b8e0b7dc1856c896dea7a34e312b5f
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2013-12-04 20:15:09 -0800

    net-client.t.c: added test for type declaration of net_client_t

---
+++ net-client/net-client.h
@@ -0,0 +1,8 @@
+#ifndef __NET_CLIENT_H
+#define __NET_CLIENT_H
+
+typedef struct net_client {
+	char * target;
+} net_client_t;
+
+#endif
--- net-client/net-client.t.c
+++ net-client/net-client.t.c
@@ -1,5 +1,12 @@
+#include "net-client.c"
+
 #include <stdlib.h>
+#include <assert.h>
 
 int main(void) {
+
+	net_client_t * client = NULL;
+	assert(client == NULL);
+
 	return EXIT_SUCCESS;
 }
